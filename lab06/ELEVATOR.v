b727436fa6402cca6e13987484c50b5ab79fcc41
Joyqul Chen <joy1357924680@gmail.com>
2013-12-03 18:50:33 +0800

    add lab07 pdf

---
+++ lab06/ELEVATOR.v
@@ -0,0 +1,13 @@
+module ELEVATOR(FLOOR, OPEN, CLK, RESET, PEOPLE, GOTO);
+
+    input            CLK, RESET, PEOPLE; 
+    input      [2:0] GOTO;
+    output reg       OPEN;   
+    output reg [2:0] FLOOR;
+
+
+
+
+
+
+endmodule
